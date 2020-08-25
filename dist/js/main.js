8a841a36cda1edb12c64df2c4c1bdced3c1f43ca
Hunter Matthews <sirhunty@gmail.com>
2020-08-25 15:01:29 -0700

    fixed menu branding class name for the flex comlumn

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.branding');
+const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 // setting of the intial states (true and false)
