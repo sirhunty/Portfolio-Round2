f74de1ca0616af6e3db69c2910f45ca67c1d086e
Hunter Matthews <sirhunty@gmail.com>
2020-09-04 18:58:01 -0700

    added the feature for the user to be able to click off the modal screen to close it as well as the X feature

--- src/public/js/modal.js
+++ src/public/js/modal.js
@@ -19,3 +19,10 @@ var span = document.getElementsByClassName("closeModal")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
