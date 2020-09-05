a0c3bab5f98f8dbb35ccc50df240f73d198d0ac8
Hunter Matthews <sirhunty@gmail.com>
2020-09-04 18:38:09 -0700

    added j query for multiple modals; set up works for one image so far, testing multiple on next push

--- src/public/js/modal.js
+++ src/public/js/modal.js
@@ -1,18 +1,19 @@
 // Get the modal
-let modal = document.getElementById("trailModal");
+let modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.getElementById("trailImg");
-let modalImg = document.getElementById("img02");
+let img = $('.myImg');
+let modalImg = $("#img01");
 let captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    let newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("closeModal")[0];
+var span = document.getElementsByClassName("closeModal")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
