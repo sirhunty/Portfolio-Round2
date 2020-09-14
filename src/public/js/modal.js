716c74f5ea80413ab734c2c132ae06fb4e34557f
Hunter Matthews <sirhunty@gmail.com>
2020-09-14 16:57:12 -0700

    added functionality to modal js to display the p text for the given modal clicked

--- src/public/js/modal.js
+++ src/public/js/modal.js
@@ -6,6 +6,7 @@ let galleryItem = $('.item');
 let img = $('.myImg');
 let text = $('.modalInfo');
 let modalImg = $("#img01");
+let modalDesc = $("#modalDesc");
 let modalText = document.getElementsByClassName("modalInfo");
 let captionText = document.getElementById("caption");
 $('.myImg').click(function(){
@@ -13,6 +14,8 @@ $('.myImg').click(function(){
     let newSrc = this.src;
     modalImg.attr('src', newSrc);
     captionText.innerHTML = this.alt;
+    var description = $(this).parent().children(".modalInfo")[0].innerHTML;
+    modalDesc.text(description);
 });
 
 // Get the <span> element that closes the modal
